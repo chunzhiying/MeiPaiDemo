994334be0a1feabcee0295336d318d8a42641bec
chunzhiying <chun.zhiying.ggl@gmail.com>
2015-09-29 16:47:39 +0800

    init

---
+++ MeiPaiDemo/MeiPaiDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeiPaiDemo
+//
+//  Created by 陈智颖 on 15/9/29.
+//  Copyright © 2015年 YY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
